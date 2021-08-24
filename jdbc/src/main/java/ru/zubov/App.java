1ad5dc119c9ac53e6acf60dcde12cf329887fc6e
ZubovVP <Zubov.VP@yandex.ru>
2021-08-24 23:50:59 +0300

    Create module for jdbc.

---
+++ jdbc/src/main/java/ru/zubov/App.java
@@ -0,0 +1,13 @@
+package ru.zubov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
